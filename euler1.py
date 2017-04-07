4570eb8470ec42d3788eb5496aa9bf4b5cf389fa
Tobias Pfeiffer <tgp@preferred.jp>
2017-04-07 13:40:52 +0900

    add the program

---
+++ euler1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+
+# This program is an attempt at solving
+#   https://projecteuler.net/problem=1
+# for educational purposes.
+
+if not len(sys.argv) == 2:
+    sys.stderr.write("usage: %s <n>\n")
+    sys.exit(1)
+
+try:
+    n = int(sys.argv[1])
+except ValueError:
+    sys.stderr.write("program argument must be an integer\n")
+    sys.exit(1)
+
+result = 0
+
+for i in xrange(n):
+    # can you spot the bug in this loop?
+    if i % 3 == 0:
+        result += i
+    if i % 5 == 0:
+        result += i
+
+print result
+
